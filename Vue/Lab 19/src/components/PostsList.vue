dd94ad010c6a56c1711b2423da3feaabf539950d
NordicFlame <christer.sjoholm@gmail.com>
2022-12-16 16:19:07 +0100

    lab 19 search

--- Vue/Lab 19/src/components/PostsList.vue
+++ Vue/Lab 19/src/components/PostsList.vue
@@ -2,72 +2,46 @@
   <p v-if="error">
     {{error}}
   </p>
-  <!-- <div v-if="loading"><Loader /></div> -->
   <div v-if="posts.length">
     <BlogPost v-for="post in posts" :key="post.id" :post="post" />
   </div>
   <p v-else>No posts to show</p>
 </template>
 
-<!-- <script setup>
-import { ref } from 'vue'
+<script setup>
+import { ref, watch } from 'vue'
 import axios from 'axios'
-import Loader from './Loader.vue'
 import BlogPost from './BlogPost.vue'
-  //http://localhost:3000/posts
+import { useRoute } from 'vue-router';
+
   const posts = ref([])
-  const error = ref(null)
-  const loading = ref(false)
+  const error = ref(null)  
+  const route = useRoute()
 
   const getPosts = async () => {
-    loading.value = true
     try {
       error.value = null
-      const res = await axios.get('http://localhost:3000/posts')
+      console.log('query = ' + route.query.searchQuery)
+
+      const url = route.query.searchQuery 
+      ? `http://localhost:3000/posts?q=${route.query.searchQuery}`
+      : 'http://localhost:3000/posts'
+
+      const res = await axios.get(url)
       if(res.status !== 200) {
         throw new Error(res.statusText)
       }
 
       posts.value = res.data
-      loading.value = false
+      
     } catch (err) {
       console.log(err.message)
       error.value = "Det gick inte att hämta inläggen"
-      loading.value = false
-    }
-    
+    }    
   }
-
-  getPosts()
+  await getPosts()
+  watch(() => route.query, getPosts)
 </script> -->
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-import Loader from './Loader.vue'
-import BlogPost from './BlogPost.vue'
-  //http://localhost:3000/posts
-  const posts = ref([])
-  const error = ref(null)
-  const loading = ref(false)
-
-    loading.value = true
-    try {
-      error.value = null
-      const res = await axios.get('http://localhost:3000/posts')
-      if(res.status !== 200) {
-        throw new Error(res.statusText)
-      }
-
-      posts.value = res.data
-      loading.value = false
-    } catch (err) {
-      console.log(err.message)
-      error.value = "Det gick inte att hämta inläggen"
-      loading.value = false
-    }
-    
-
-</script>
 
 <style scoped>
 
